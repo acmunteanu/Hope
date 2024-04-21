3ed1389a07679079001007e55b64bd8543e23f6a
Cosmin Munteanu <gosmin.c@gmail.com>
2024-04-21 22:09:13 +0300

    9th commit

--- Source_Code/Hope/player.py
+++ Source_Code/Hope/player.py
@@ -5,6 +5,7 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, position, image_path, controls):
         super().__init__()
         self.image = pygame.image.load(image_path).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (50, 50))
         self.rect = self.image.get_rect(midbottom = position)
         self.speed = 5
         self.score = 0
